c5f20c5b7a607f2cfbb424862275b8e1380d1e21
origin <wjdalsckd777@naver.com>
2021-04-27 22:34:31 +0900

    modified get all posts frontend

--- frontend/bloom-client-app/src/post/Post.js
+++ frontend/bloom-client-app/src/post/Post.js
@@ -6,7 +6,7 @@ import { formatDateTime } from '../util/Helpers';
 import { RadioGroup } from '@material-ui/core';
 
 
-function Post({posts}) {
+function Post({post}) {
     const [content, setContent ] = useState('')
     const [images, setImages] = useState(null);
     const [createdBy, setCreatedBy]= useState({
@@ -19,11 +19,46 @@ function Post({posts}) {
     const [totalLikes, setTotalLikes] = useState(null);
 
     useEffect(() => {
-        console.log("posts : " +posts);
-    },[])
+        // console.log(post);
+        // const image = post.images;
+        // post.images.map((image) => {
+        //     console.log(image.imageId)
+        //     console.log(image.data)
+        // })
+    }, [])
+
     return (
         <div className="post-content">
-            <h2>Post</h2>
+            <div className="post-header">
+                <div className="post-creator-info">
+                    <Link className="creator-link">
+                        <Avatar className="post-creator-avatar"
+                            style={{ backgroundColor: getAvatarColor(post.createdBy.name)}}>  
+                            {post.createdBy.name}
+                        </Avatar>
+                        <span className="post-creator-name">
+                            {post.createdBy.name}
+                        </span>
+                        <span className="post-creator-username">
+                            {post.createdBy.username}
+                        </span>
+                        <span className="post-creation-date">
+                            {post.creationDateTime}
+                        </span>
+                    </Link>
+                </div>
+                <div className="post-content">
+                    {post.content}
+                </div>
+                <div className="post-image">
+                   {
+                    post.images.map((image) => {
+                        const data = image.data;
+                        <img src={`data:image/jpeg;base64,${data}`} />
+                    })
+                   }
+                </div>
+            </div>
         </div>
     );
 }
